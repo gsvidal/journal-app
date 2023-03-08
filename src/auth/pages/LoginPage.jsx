b107d8993a0bc5f72e0fc24cb703b21879c784a4
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-03-08 15:45:44 -0500

    Add AuthLayout and use it for Login and Register pages

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,54 +3,42 @@
 import GoogleIcon from '@mui/icons-material/Google';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid item className="box-shadow" xs={3} sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
-        <Typography variant="h5" sx={{ mb: 1 }}>
-          Login
-        </Typography>
+    <AuthLayout title="Login">
+      <form action="">
+        <Grid container>
+          <Grid item xs={12} sx={{ my: { xs: 2, md: 3 } }}>
+            <TextField label="Email" type="email" placeholder="email@gmail.com" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ my: { xs: 2, md: 3 } }}>
+            <TextField label="Password" type="password" placeholder="password" fullWidth />
+          </Grid>
 
-        <form action="">
-          <Grid container>
-            <Grid item xs={12} sx={{ my: 2 }}>
-              <TextField label="Email" type="email" placeholder="email@gmail.com" fullWidth />
-            </Grid>
-            <Grid item xs={12} sx={{ my: 2 }}>
-              <TextField label="Password" type="password" placeholder="password" fullWidth />
+          <Grid container spacing={2} sx={{ my: 2 }}>
+            <Grid item xs={12} sm={6}>
+              <Button sx={{ py: 2 }} variant="contained" fullWidth>
+                <Typography>Login</Typography>
+              </Button>
             </Grid>
 
-            <Grid container spacing={2} sx={{ my: 2 }}>
-              <Grid item xs={12} sm={6}>
-                <Button sx={{py: 2}} variant="contained" fullWidth>
-                  <Typography>Login</Typography>
-                </Button>
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <Button  sx={{py: 2}} variant="contained" fullWidth>
-                  <GoogleIcon />
-                    <Typography sx={{ml: 1}}>Google</Typography>
-                </Button>
-              </Grid>
-            </Grid>
-
-            <Grid container direction='row' justifyContent='end'>
-              <Link component={ RouterLink } color='inherit' to='/auth/register'>
-                Create account
-              </Link>
+            <Grid item xs={12} sm={6}>
+              <Button sx={{ py: 2 }} variant="contained" fullWidth>
+                <GoogleIcon />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
             </Grid>
           </Grid>
-        </form>
-      </Grid>
-    </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Create account
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   );
 };
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,38 @@
-//import './RegisterPage.css';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const RegisterPage = () => {
   return (
-    <>
-      <div>RegisterPage</div>
-    </>
+    <AuthLayout title="Register">
+      <form action="">
+        <Grid container>
+          <Grid item xs={12} sx={{ my: { xs: 2, md: 3 } }}>
+            <TextField label="Name" type="text" placeholder="Your full name" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ my: { xs: 2, md: 3 } }}>
+            <TextField label="Email" type="email" placeholder="email@gmail.com" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ my: { xs: 2, md: 3 } }}>
+            <TextField label="Password" type="password" placeholder="password" fullWidth />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ my: 2 }}>
+            <Grid item xs={12}>
+              <Button sx={{ py: 2 }} variant="contained" fullWidth>
+                <Typography>Create account</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{mr: 2}}>Already have an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Log in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   );
 };
