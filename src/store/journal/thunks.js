bba4465b5cdc197b76ec5c0513045440c578c712
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-07-26 21:33:00 -0500

    Create newNote id property

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,20 +1,22 @@
-import { collection, doc, setDoc } from 'firebase/firestore/lite';
-import { FirebaseDB } from '../../firebase/config';
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
+    // console.log(getState())
     const { uid } = getState().auth;
 
     const newNote = {
-      title: '',
-      body: '',
+      title: "",
+      body: "",
       date: new Date().getTime(),
     };
 
     const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
     const setDocResponse = await setDoc(newDoc, newNote);
 
-    console.log({ newDoc, setDocResponse });
+    // console.log({ newDoc, setDocResponse });
+    newNote.id = newDoc.id;
 
     // dispatch
     // dispatch(newNote)
